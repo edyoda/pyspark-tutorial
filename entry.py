a2aafdfc3227cc52394e68b3c620b008cd8cc952
Awantik Das <awantikdas@gmail.com>
2019-03-06 13:19:44 +0800

    Add files via upload

--- entry.py
+++ entry.py
@@ -1,29 +1,9 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import *
-import pyspark.sql.functions as func
+
 import argparse
 
-
-
-
-if __name__ == '__main__':
-
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--input",  help="Path to file/directory for training data", required=True)
-    params = parser.parse_args()
-
-    spark = SparkSession \
-        .builder \
-        .appName('PySpark-App') \
-        .getOrCreate()
-
-    print('Session created')
-
-    try:
-        data = spark.read.csv(params.input,header=True,inferSchema=True)
-        print ('Final Count ', data.count())
-
-    finally:
-        spark.stop()
+if __name__ == '__main__': 
+    spark = SparkSession.builder.appName('PySpark-App').getOrCreate() 
+    print('Session created') 
+    emp_data = spark.createDataFrame([(1,2),(3,4),(5,6)],['a','b'])
+    print (emp_data.count())
